448c43c826f8962ed84104e58d973c31a429056c
Hung2113 <117303780+Hung2113@users.noreply.github.com>
2022-11-03 16:54:44 +0700

    xây dựng trang login, validate, truyền dữ liệu

--- app/src/main/java/com/example/quanlinhanvien/MainActivity.java
+++ app/src/main/java/com/example/quanlinhanvien/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.quanlinhanvien;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        test truyền dữ liệu
+        tv = findViewById(R.id.tv);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String e = bundle.getString("email");
+        String ep= bundle.getString("email");
+        tv.setText(bundle.getString("email")+"     "+bundle.getString("password"));
+
+         */
     }
 }

6e66ae86dd50854e0533b24c62c49bc0c3980e52
Baor <duongdinhhoangbao68@gmail.com>
2022-11-05 18:08:00 +0700

    chinh lai main ativity

--- app/src/main/java/com/example/quanlinhanvien/MainActivity.java
+++ app/src/main/java/com/example/quanlinhanvien/MainActivity.java
@@ -41,12 +41,57 @@ public class MainActivity extends AppCompatActivity {
         }
 
         //hien thi ngay
+        String thuchu="vv";
+        String thangchu="vv";
 
        Calendar n = Calendar.getInstance();
         int year = n.get(Calendar.YEAR);
         int month = n.get(Calendar.MONTH);
         int day = n.get(Calendar.DAY_OF_MONTH);
-        ngayht.setText("Ngày"+day+",tháng"+month+" năm "+year);
+        int thu = n.get(Calendar.DAY_OF_WEEK);
+        switch (thu){
+            case 2 : thuchu ="Monday";
+            break;
+            case 3 : thuchu ="Tuesday ";
+                break;
+            case 4 : thuchu ="Wednesday ";
+                break;
+            case 5 : thuchu ="Thursday";
+                break;
+            case 6 : thuchu ="Friday ";
+                break;
+            case 7 : thuchu ="Saturday ";
+                break;
+            case 1 : thuchu ="Sunday ";
+                break;
+        }
+        switch (month+1){
+            case 1 : thangchu="Jan";
+            break;
+            case 2 : thangchu="Feb";
+                break;
+            case 3 : thangchu="Mar";
+                break;
+            case 4 : thangchu="Apr";
+                break;
+            case 5 : thangchu="May";
+                break;
+            case 6 : thangchu="Jun";
+                break;
+            case 7 : thangchu="Jul";
+                break;
+            case 8 : thangchu="Aug";
+                break;
+            case 9 : thangchu="Sep";
+                break;
+            case 10 : thangchu="Oct";
+                break;
+            case 11 : thangchu="Nov";
+                break;
+            case 12 : thangchu="Dec";
+                break;
+        }
+        ngayht.setText(thuchu+","+day+" "+thangchu+" "+year);
 
        //
         Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.hien);

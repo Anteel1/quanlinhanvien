744fe309daa15e3c793bd5c9ef5ff2ec232163ca
Hung2113 <117303780+Hung2113@users.noreply.github.com>
2022-11-06 18:58:09 +0700

    custom xong toolbar

--- app/src/main/java/com/example/quanlinhanvien/manhinhchoActivity.java
+++ app/src/main/java/com/example/quanlinhanvien/manhinhchoActivity.java
@@ -1,18 +1,19 @@
 package com.example.quanlinhanvien;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class manhinhchoActivity extends AppCompatActivity {
     ProgressBar progressBar;
     TextView textView;
     Handler handler = new Handler();
     int i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +25,13 @@ public class manhinhchoActivity extends AppCompatActivity {
             public void run() {
                 chay();
                 startActivity(new Intent(manhinhchoActivity.this, LoginActivity.class));
+                finish();
+
             }
         });
         thread.start();
+
+
     }
 
     public void chay() {

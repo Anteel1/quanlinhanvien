c5573cf31d24b4eef3fa5160f4b3518b99179b43
Synxndh <ndhs824@gmail.com>
2022-11-06 09:50:24 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/quanlinhanvien/MainActivity.java
+++ app/src/main/java/com/example/quanlinhanvien/MainActivity.java
@@ -21,35 +21,27 @@ public class MainActivity extends AppCompatActivity {
         TextView ngayht = findViewById(R.id.ngayhienthi);
         ImageButton nen = findViewById(R.id.nen);
 
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR);
+        int minute = c.get(Calendar.MINUTE);
+        int gio = c.get(Calendar.HOUR_OF_DAY);
 
-        //hien thi gio
+        if (gio >= 12) gioht.setText(hour + ":" + minute + "PM");
+        else gioht.setText(hour + ":" + minute + "AM");
 
-                final Calendar c = Calendar.getInstance();
-                int hour = c.get(Calendar.HOUR);
-                int minute = c.get(Calendar.MINUTE);
-                int gio = c.get(Calendar.HOUR_OF_DAY);
-                if (gio >= 12) {
-                    gioht.setText(hour + ":" + minute + "PM");
-                } else {
-                    gioht.setText(hour + ":" + minute + "AM");
-                }
         if (minute <10){
-            if (gio >= 12) {
-                gioht.setText(hour + ":0" + minute + "PM");
-            } else {
-                gioht.setText(hour + ":0" + minute + "AM");
-          }
+            if (gio >= 12) gioht.setText(hour + ":0" + minute + "PM");
+            else gioht.setText(hour + ":0" + minute + "AM");
         }
 
-        //hien thi ngay
+
 
        Calendar n = Calendar.getInstance();
         int year = n.get(Calendar.YEAR);
         int month = n.get(Calendar.MONTH);
         int day = n.get(Calendar.DAY_OF_MONTH);
-        ngayht.setText("Ngày"+day+",tháng"+month+" năm "+year);
+        ngayht.setText("Ngày " + day + " tháng " + month + " năm " + year);
 
-       //
         Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.hien);
         nen.startAnimation(animation);
         }

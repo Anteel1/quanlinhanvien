65fe79639ba3d0c8c7d9bd7d5a74cff326c5d458
Synxndh <ndhs824@gmail.com>
2022-11-06 09:56:10 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/quanlinhanvien/MainActivity.java
+++ app/src/main/java/com/example/quanlinhanvien/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Calendar;
-
+//Sydeptrai
 public class MainActivity extends AppCompatActivity {
     TextView tv;
     @Override

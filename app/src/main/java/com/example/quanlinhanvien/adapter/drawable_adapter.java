8db10770390218f1d3bd658054d767a28f8e752c
Hung2113 <117303780+Hung2113@users.noreply.github.com>
2022-11-15 01:17:49 +0700

    Update drawable_adapter.java

--- app/src/main/java/com/example/quanlinhanvien/adapter/drawable_adapter.java
+++ app/src/main/java/com/example/quanlinhanvien/adapter/drawable_adapter.java
@@ -1,29 +1,57 @@
 package com.example.quanlinhanvien.adapter;
 
+import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class drawable_adapter extends RecyclerView.Adapter<drawable_adapter.ViewHolder>  {
 
-    
+    private List<drawable_item> items;
+    private Map<Class<? extends drawable_item>, Integer> viewtype;
+    private SparseArray<drawable_adapter> holderFactories;
+
+    private AdapterView.OnItemSelectedListener listener;
+
+    public drawable_adapter (List<drawable_item> items){
+        this.items = items;
+        this.viewtype = new HashMap<>();
+        this.holderFactories = new SparseArray<>();
+        processViewTypes();
+    }
+
+    private void processViewTypes() {
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        ViewHolder holder = holderFactories.get(viewType).createViewHolder(parent);
+        holder.drawable_adapter = this;
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        items.get(position).onbindViewHolder(holder);
+    }
 
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
     static abstract class Viewholder extends RecyclerView.ViewHolder implements View.OnClickListener{
@@ -42,6 +70,8 @@ public class drawable_adapter extends RecyclerView.Adapter<drawable_adapter.View
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public com.example.quanlinhanvien.adapter.drawable_adapter drawable_adapter;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
         }

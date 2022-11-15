7f3b35cd1ad25c9c1cf9e9dc48867ca138da9bb0
trunghau04 <hauoanh3004@gmail.com>
2022-11-15 11:18:55 +0700

    hoàn thành dialog logout

--- app/src/main/java/com/example/quanlinhanvien/fragment/frm_dangxuat.java
+++ app/src/main/java/com/example/quanlinhanvien/fragment/frm_dangxuat.java
@@ -1,17 +1,50 @@
 package com.example.quanlinhanvien.fragment;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import com.example.quanlinhanvien.LoginActivity;
+import com.example.quanlinhanvien.MainActivity;
 import com.example.quanlinhanvien.R;
 
-public class frm_dangxuat extends Fragment {
+public class frm_dangxuat extends DialogFragment {
+    Button dialogYes;
+    Button dialogNo;
+    Dialog dialog;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.frm_dangxuat);
+        dialogYes = (Button) dialog.findViewById(R.id.btnYes);
+        dialogNo = (Button) dialog.findViewById(R.id.btnNo);
+        dialogYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), LoginActivity.class));
+            }
+        });
+        dialogNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+        return dialog;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,4 +52,5 @@ public class frm_dangxuat extends Fragment {
 
         return view;
     }
+
 }

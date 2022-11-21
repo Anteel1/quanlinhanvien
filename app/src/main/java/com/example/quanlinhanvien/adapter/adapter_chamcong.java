b91e0074121c1b9a81656cb07826acadde739d6e
Baor <duongdinhhoangbao68@gmail.com>
2022-11-21 16:51:30 +0700

    chamcong2

---
+++ app/src/main/java/com/example/quanlinhanvien/adapter/adapter_chamcong.java
@@ -0,0 +1,33 @@
+package com.example.quanlinhanvien.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class adapter_chamcong extends BaseAdapter {
+    ArrayList<chamcong>
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
